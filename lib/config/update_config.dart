fe570ccd7343eee116be85ec62ab14dac4e39b91
Hillah Ayite Essenam <josh@Hillahs-Mac-mini.local>
2025-12-27 09:04:53 +0000

    Fix: Automate versioning and release workflow for mobile app auto-update

--- lib/config/update_config.dart
+++ lib/config/update_config.dart
@@ -7,5 +7,5 @@ class UpdateConfig {
   ///   "forceUpdate": false,
   ///   "releaseNotes": "Fixes payment bug and adds new features."
   /// }
-  static const String versionCheckUrl = 'https://raw.githubusercontent.com/Josh84-Axe/Flutter_partnerapp/fix/token-storage-windows/version.json';
+  static const String versionCheckUrl = 'https://raw.githubusercontent.com/Josh84-Axe/Flutter_partnerapp/main/version.json';
 }
