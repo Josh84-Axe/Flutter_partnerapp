1c52030fbed935512a58610fe7f90ea4c5a284c2
Hillah Ayite Essenam <josh@Hillahs-Mac-mini.local>
2026-02-09 22:57:47 +0000

    Bump to v1.0.8 - Add Visual Debug Banner to index.html

--- lib/main.dart
+++ lib/main.dart
@@ -127,7 +127,7 @@ import 'screens/active_sessions_screen.dart';
 import 'screens/help_support_screen.dart';
 
 void main() async {
-  print('🚀 WIFI-4U PARTNER APP - FLUTTER RUN v1.0.7');
+  print('🚀 WIFI-4U PARTNER APP - FLUTTER RUN v1.0.8');
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   
