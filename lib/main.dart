821592bf5dfe0e85cc3b57f19c231ec8a3811631
Hillah Ayite Essenam <josh@Hillahs-Mac-mini.local>
2026-02-10 09:19:08 +0000

    Fix: Add missing dio import to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:dio/dio.dart'; // Added for Dio type
 
 import 'package:easy_localization/easy_localization.dart';
 
