c205f4dc0921bcb5694619292d10275b49dad7c0
Hillah Ayite Essenam <josh@Hillahs-Mac-mini.local>
2026-03-02 21:17:21 +0000

    fix: restore accidentally removed flutter/material.dart imports

--- lib/screens/assign_router_screen.dart
+++ lib/screens/assign_router_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:easy_localization/easy_localization.dart';
 import '../providers/split/network_provider.dart';
