1a1945f5773e1587b0ce480d61cae43f692f485c
Hillah Ayite Essenam <josh@Hillahs-Mac-mini.local>
2026-08-08 19:56:40 +0000

    fix(csp): add http: and https: to connect-src directive in web/index.html BUILD 1.2.263

--- web/service-worker.js
+++ web/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tiknet-pwa-v1.2.262';
+const CACHE_NAME = 'tiknet-pwa-v1.2.263';
 const RESOURCES_TO_CACHE = [
     './',
     './index.html',
@@ -10,7 +10,7 @@ const RESOURCES_TO_CACHE = [
 
 // Install Event
 self.addEventListener('install', (event) => {
-    console.log('📦 [Service Worker] Installing v1.2.262');
+    console.log('📦 [Service Worker] Installing v1.2.263');
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(RESOURCES_TO_CACHE);
