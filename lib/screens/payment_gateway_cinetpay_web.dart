820ef2acea8c9cc03f9f7ddddd90f0b5fef5b9c5
Hillah Ayite Essenam <josh@Hillahs-Mac-mini.local>
2026-08-22 22:52:00 +0000

    fix(payment): add full API keys, site ID, transaction ID, and dynamic Togo/CI location parameters for CinetPay

--- lib/screens/payment_gateway_cinetpay_web.dart
+++ lib/screens/payment_gateway_cinetpay_web.dart
@@ -61,16 +61,26 @@ class _PaymentGatewayCinetPayState extends State<PaymentGatewayCinetPay> {
   void _launchCinetPay() {
     try {
       final jsData = JSObject();
+      final txnId = 'txn_${DateTime.now().millisecondsSinceEpoch}';
+
+      jsData.setProperty('apiKey'.toJS, '297929662685d35c4021b02.21438964'.toJS);
+      jsData.setProperty('siteId'.toJS, '105899723'.toJS);
+      jsData.setProperty('notifyUrl'.toJS, 'https://staging.wifi-4u.net/v1/webhooks/cinetpay/'.toJS);
+      jsData.setProperty('transactionId'.toJS, txnId.toJS);
       jsData.setProperty('amount'.toJS, widget.amount.toJS);
-      jsData.setProperty('currency'.toJS, widget.currency.toJS);
+      jsData.setProperty('currency'.toJS, (widget.currency == 'CFA' ? 'XOF' : widget.currency).toJS);
       jsData.setProperty('description'.toJS, widget.description.toJS);
       jsData.setProperty('email'.toJS, widget.email.toJS);
+      jsData.setProperty('customerName'.toJS, (widget.firstName.isNotEmpty ? widget.firstName : 'Customer').toJS);
+      jsData.setProperty('customerSurname'.toJS, (widget.lastName.isNotEmpty ? widget.lastName : 'User').toJS);
+      jsData.setProperty('customerEmail'.toJS, widget.email.toJS);
+      jsData.setProperty('customerPhoneNumber'.toJS, (widget.phoneNumber.isNotEmpty ? widget.phoneNumber : '+22890000000').toJS);
       jsData.setProperty('firstName'.toJS, widget.firstName.toJS);
       jsData.setProperty('lastName'.toJS, widget.lastName.toJS);
       jsData.setProperty('phone'.toJS, widget.phoneNumber.toJS);
       jsData.setProperty('address'.toJS, widget.address.toJS);
-      jsData.setProperty('city'.toJS, widget.city.toJS);
-      jsData.setProperty('country'.toJS, widget.country.toJS);
+      jsData.setProperty('city'.toJS, (widget.city.isNotEmpty ? widget.city : (widget.country == 'TG' ? 'Lomé' : 'Abidjan')).toJS);
+      jsData.setProperty('country'.toJS, (widget.country.isNotEmpty ? widget.country : 'TG').toJS);
 
       globalContext.callMethod('launchCinetPay'.toJS, jsData);
     } catch (e) {
